03b520a23abec7a55b863c061a756615250c9c6f
Tristan Struthers <trstruth@microsoft.com>
2019-11-13 03:36:19 +0000

    Commit first implementation of streaming ingest

--- client.go
+++ client.go
@@ -110,3 +110,61 @@ func (c *Client) generateNewQueryRequest(query, database string) (*http.Request,
 
 	return req, nil
 }
+
+// IngestData performs streaming ingest of `data` into `database`
+func (c *Client) IngestData(data, database, table, mappingName string) (*QueryResult, error) {
+	err := c.connection.servicePrincipalToken.EnsureFresh()
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := c.generateNewIngestRequest(data, database, table, mappingName)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New(resp.Status)
+	}
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	queryResult := QueryResult{}
+	err = json.Unmarshal(bodyBytes, &queryResult)
+	if err != nil {
+		return nil, err
+	}
+
+	return &queryResult, nil
+}
+
+// generateNewIngestRequest is a helper used by IngestData to generate a request to
+// ingest data into kusto
+func (c *Client) generateNewIngestRequest(data, database, table, mappingName string) (*http.Request, error) {
+	dataBytes := []byte(data)
+	req, err := http.NewRequest(
+		"POST",
+		fmt.Sprintf("%s/v1/rest/ingest/%s/%s?streamFormat=Json&mappingName=%s", c.connection.url, database, table, mappingName),
+		bytes.NewBuffer(dataBytes),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// required headers
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.connection.servicePrincipalToken.Token().AccessToken))
+	req.Header.Set("Host", c.connection.url.Hostname())
+
+	// optional headers
+
+	return req, nil
+}
