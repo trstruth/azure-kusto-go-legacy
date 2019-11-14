9849c150e9c6bf8c505ecfb96422b2589b88e399
Tristan Struthers <trstruth@microsoft.com>
2019-11-14 01:13:48 +0000

    Add tests for ExecuteQuery and IngestData

--- client_test.go
+++ client_test.go
@@ -112,21 +112,152 @@ func TestGenerateNewIngestRequest(t *testing.T) {
 
 	for headerKey, expectedHeaderVal := range expectedHeaders {
 		actualHeaderVal := req.Header[headerKey]
-		if !stringSlicesAreEqual(actualHeaderVal, expectedHeaderVal) {
-			t.Fatalf("the key %s in the generated request was incorrect: want %s, got %s", headerKey, expectedHeaderVal, actualHeaderVal)
+		if !reflect.DeepEqual(actualHeaderVal, expectedHeaderVal) {
+			t.Errorf("the key %s in the generated request was incorrect: want %s, got %s", headerKey, expectedHeaderVal, actualHeaderVal)
 		}
 	}
 }
 
-func stringSlicesAreEqual(a, b []string) bool {
-	if len(a) != len(b) {
-		return false
+func TestExecuteQuery(t *testing.T) {
+	conn, err := NewConnectionWithAADApplicationKeyAuth(
+		clusterName,
+		clientID,
+		secret,
+		tenantID,
+	)
+	if err != nil {
+		t.Errorf(err.Error())
 	}
 
-	for i := range a {
-		if a[i] != b[i] {
-			return false
+	KustoAPIRequestMock := RoundTripFunc(func(req *http.Request) (*http.Response, error) {
+		fileBytes, err := ioutil.ReadFile("testdata/ExecuteQueryResponseBody.json")
+		if err != nil {
+			return nil, err
 		}
+
+		mockResp := &http.Response{
+			Status:     "200 OK",
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBuffer(fileBytes)),
+		}
+		return mockResp, nil
+	})
+	mockHTTPClient := &http.Client{
+		Transport: KustoAPIRequestMock,
 	}
-	return true
+
+	c := NewClient(conn, SetHTTPClient(mockHTTPClient))
+
+	// disable spt auto refresh to prevent the spt from making a real http request
+	// to the aad endpoint with our test clientID/secret
+	c.connection.servicePrincipalToken.SetAutoRefresh(false)
+
+	result, err := c.ExecuteQuery(query, database)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	resultTable := result.Tables[0]
+
+	expectedColumns := []Column{
+		Column{"Col", "String"},
+		Column{"Col2", "String"},
+		Column{"Col3", "Int32"},
+		Column{"Col4", "DateTime"},
+	}
+
+	expectedRows := []Row{
+		Row{nil, nil, nil, nil},
+		// note the use of float here - it seems that despite Kustos best attempt to annotate
+		// the types via the columns object in the response, go's JSON unmarshaller defaults
+		// to unmarshalling numbers to go's float64
+		Row{"test", "2k", float64(123), "2019-10-14T03:10:07.193296Z"},
+	}
+
+	if !reflect.DeepEqual(expectedColumns, resultTable.Columns) {
+		t.Errorf("Columns of result table did not match: want %s, got %s", expectedColumns, resultTable.Columns)
+	}
+
+	if !reflect.DeepEqual(expectedRows, resultTable.Rows) {
+		t.Errorf("Rows of result table did not match: want %s, got %s", expectedRows, resultTable.Rows)
+	}
+}
+
+func TestIngestData(t *testing.T) {
+	conn, err := NewConnectionWithAADApplicationKeyAuth(
+		clusterName,
+		clientID,
+		secret,
+		tenantID,
+	)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	KustoAPIRequestMock := RoundTripFunc(func(req *http.Request) (*http.Response, error) {
+		fileBytes, err := ioutil.ReadFile("testdata/IngestDataResponseBody.json")
+		if err != nil {
+			return nil, err
+		}
+
+		mockResp := &http.Response{
+			Status:     "200 OK",
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBuffer(fileBytes)),
+		}
+		return mockResp, nil
+	})
+	mockHTTPClient := &http.Client{
+		Transport: KustoAPIRequestMock,
+	}
+
+	c := NewClient(conn, SetHTTPClient(mockHTTPClient))
+
+	// disable spt auto refresh to prevent the spt from making a real http request
+	// to the aad endpoint with our test clientID/secret
+	c.connection.servicePrincipalToken.SetAutoRefresh(false)
+
+	data := `{"Col": "Tristan","Col2": "Test","Col3": 123,"Col4": "2019-10-14 03:10:07.1932960"}`
+	database := ""
+	table := "TestTable"
+	mappingName := "Mapping1"
+
+	result, err := c.IngestData(data, database, table, mappingName)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	resultTable := result.Tables[0]
+
+	expectedColumns := []Column{
+		Column{"ConsumedRecordsCount", "Int64"},
+		Column{"UpdatePolicyStatus", "String"},
+		Column{"UpdatePolicyFailureCode", "String"},
+		Column{"UpdatePolicyFailureReason", "String"},
+	}
+
+	expectedRows := []Row{
+		// note the use of float here - it seems that despite Kustos best attempt to annotate
+		// the types via the columns object in the response, go's JSON unmarshaller defaults
+		// to unmarshalling numbers to go's float64
+		Row{float64(0), "Inactive", "Unknown", nil},
+	}
+
+	if !reflect.DeepEqual(expectedColumns, resultTable.Columns) {
+		t.Errorf("Columns of result table did not match: want %s, got %s", expectedColumns, resultTable.Columns)
+	}
+
+	if !reflect.DeepEqual(expectedRows, resultTable.Rows) {
+		t.Errorf("Rows of result table did not match: want %s, got %s", expectedRows, resultTable.Rows)
+	}
+}
+
+// RoundTripFunc is the function which will mock the actual req/resp
+// round trip
+type RoundTripFunc func(*http.Request) (*http.Response, error)
+
+// RoundTrip is a method of the function type RoundTripFunc, such that
+// it satisfies the RoundTripper interface
+func (fn RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return fn(req)
 }
